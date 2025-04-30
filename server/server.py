df0059a6fb70893f7b1d083856329e90416ce74a
TheRepik-379 <TheRepik@gmail.com>
2025-04-30 21:12:33 +0500

    Second commit

--- server/server.py
+++ server/server.py
@@ -1,84 +1,80 @@
 from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
 from sqlalchemy import create_engine, text, bindparam
-connection_string = "mysql+pymysql://admin:123@192.168.0.106:3306/article"
+
+connection_string = "mysql+pymysql://flowers:123456@192.168.200.109:3306/flowers"
 engine = create_engine(connection_string, echo=True)
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 @app.route("/")
 def index():
     return "Hello world"
-@app.route("/api/article/all")
-def get_article():
+
+@app.route("/api/product/all")
+def get_products():
     with engine.connect() as connection:
-        raw_article = connection.execute(text("SELECT * FROM article"))
-        article = []
-        for i in raw_article.all():
-            article.append(i._asdict())
-        return jsonify(article)
+        raw_result = connection.execute(text("SELECT * FROM products"))
+        result = []
+        for r in raw_result.all():
+            result.append(r._asdict())
+        return jsonify(result)
     return Response(jsonify({"status": "500", "message": "Database is down!"}), status=500)
 
-@app.route("/api/article", methods=["POST"])
-def add_article():
+@app.route("/api/product", methods=["POST"])
+def add_product():
     if request.method == "POST":
         form = request.form
         with engine.connect() as connection:
-            query = text("INSERT INTO article (Heading, description, picture, dfc) VALUES (:Heading, :description, :picture, :dfc) RETURNING *")
-            query = query.bindparams(bindparam("Heading", form.get("Heading")))
+            query = text("INSERT INTO products (name, description, price, photo) VALUES (:name, :description, :price, :photo) RETURNING *")
+            query = query.bindparams(bindparam("name", form.get("name")))
             query = query.bindparams(bindparam("description", form.get("description")))
-            query = query.bindparams(bindparam("picture", form.get("picture")))
-            query = query.bindparams(bindparam("dfc", form.get("dfc")))
+            query = query.bindparams(bindparam("price", form.get("price")))
+            query = query.bindparams(bindparam("photo", form.get("image")))
             result = connection.execute(query)
             connection.commit()
             return jsonify(result.fetchone()._asdict())
         return jsonify({"message": "Error"})
-@app.route("/api/article/<id>", methods=["DELETE","GET","PUT"])
-def delete_article(id: int):
+
+@app.route("/api/product/<id>", methods=["GET", "DELETE", "PUT"])
+def product(id: int):
+    if request.method == "GET":
+        with engine.connect() as connection:
+            query = text("SELECT * FROM products WHERE id = :id")
+            query = query.bindparams(bindparam("id", id))
+            result = connection.execute(query)
+            return jsonify(result.fetchone()._asdict())
+        return jsonify({"message": "Error"})
     if request.method == "DELETE":
         with engine.connect() as connection:
-            query = text("DELETE FROM article WHERE id = :id")
+            query = text("DELETE FROM products WHERE id = :id;")
             query = query.bindparams(bindparam("id", id))
             result = connection.execute(query)
             connection.commit()
             return jsonify({"message": "Success", "id": id})
-        return jsonify({"message": "Записи с таким id не существует"})
-    if request.method == "GET":
-        with engine.connect() as connection:
-            query = text("SELECT * FROM article WHERE id = :id")
-            query = query.bindparams(bindparam("id", id))
-            result = connection.execute(query)
-            return jsonify(result.fetchone()._asdict())
         return jsonify({"message": "Error"})
     if request.method == "PUT":
         form = request.form
         with engine.connect() as connection:
-            query = text("UPDATE article SET Heading = :Heading, description = :description, picture = :picture, dfc = :dfc WHERE id = :id")
-            query = query.bindparams(bindparam("Heading", form.get("Heading")))
+            query = text("UPDATE products SET name = :name, description = :description, price = :price, photo = :photo WHERE id = :id")
+            query = query.bindparams(bindparam("name", form.get("name")))
             query = query.bindparams(bindparam("description", form.get("description")))
-            query = query.bindparams(bindparam("picture", form.get("picture")))
-            query = query.bindparams(bindparam("dfc", form.get("dfc")))
+            query = query.bindparams(bindparam("price", form.get("price")))
+            query = query.bindparams(bindparam("photo", form.get("image")))
             query = query.bindparams(bindparam("id", id))
             connection.execute(query)
             connection.commit()
-            query = text("SELECT * FROM article WHERE id = :id")
+            query = text("SELECT * FROM products WHERE id = :id")
             query = query.bindparams(bindparam("id", id))
             result = connection.execute(query)
             return jsonify(result.fetchone()._asdict())
         return jsonify({"message": "Error"})
 
-
-@app.route("/api/article_page/<id>", methods=["GET"])
-def get_article_page(id: int):
-    if request.method == "GET":
-        with engine.connect() as connection:
-            query = text("SELECT * FROM article WHERE id = :id")
-            query = query.bindparams(bindparam("id", id))
-            result = connection.execute(query)
-            return jsonify(result.fetchone()._asdict())
-        return jsonify({"message": "Error"})
 
 def main():
     app.run("localhost", 8000, True)
+
 if __name__ == "__main__":
     main()
